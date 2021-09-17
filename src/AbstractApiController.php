8ac806bb5f54c8f48b594d1cc66d184d84b4c737
André Baldeweg <kontakt@andrebaldeweg.de>
2021-09-17 16:07:37 +0200

    refactor response

--- src/AbstractApiController.php
+++ src/AbstractApiController.php
@@ -3,50 +3,14 @@
 namespace Baldeweg\Bundle\ApiBundle;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Baldeweg\Bundle\ApiBundle\Serializer;
 
 abstract class AbstractApiController extends AbstractController
 {
-    protected function response(array $data, int $code = 200, array $headers = []): JsonResponse
+    protected function setResponse()
     {
-        return new JsonResponse(
-            json_encode($data),
-            $code,
-            $headers,
-            true
-        );
-    }
-
-    protected function serialize($data): array
-    {
-        if (!$this->fields) {
-            throw new \Exception('Please set the fields attribute!');
-        }
-
-        $serializer = new Serializer();
-
-        return $serializer->serialize($data, $this->fields);
-    }
-
-    protected function serializeCollection(array $data): array
-    {
-        $collection = [];
-        foreach ($data as $item) {
-            $collection[] = $this->serialize($item);
-        }
-
-        return $collection;
-    }
-
-    protected function invalid(): JsonResponse
-    {
-        return $this->response(['msg' => 'NOT_VALID'], 400);
-    }
-
-    protected function deleted(): JsonResponse
-    {
-        return $this->response(['msg' => 'DELETED']);
+        return new Response(new Serializer());
     }
 
     protected function submitForm(Request $request): array
